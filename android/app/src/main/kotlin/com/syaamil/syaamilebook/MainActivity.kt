0d42115558c6b28cd4a123e8211bb1e264d568b4
muhamad.fachrulzaini@gmail.com <muhamad.fachrulzaini@gmail.com>
2020-08-04 07:09:56 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/syaamil/syaamilebook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.syaamil.syaamilebook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:syaamilebook/constants.dart';
+import 'package:syaamilebook/screens/home_screen.dart';
+import 'package:syaamilebook/widgets/rounded_button.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Book App',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.apply(
+                displayColor: kBlackColor,
+              )),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Bitmap.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.display3,
+                children: [
+                  TextSpan(text: "syaamil"),
+                  TextSpan(
+                      text: "go.",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "start reading",
+                fontSize: 20,
+                press: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return HomeScreen();
+                  }));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
